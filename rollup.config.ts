4cf8076048b4b23afe3c52b12c88f238c736722d
Alberto Piu <a@lbr.to>
2020-03-18 00:40:11 +0100

    fix: library name

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,7 +10,7 @@ const pkg = require('./package.json')
 const libraryName = 'prim'
 
 export default {
-  input: `src/${libraryName}.ts`,
+  input: `src/${camelCase(libraryName)}.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
