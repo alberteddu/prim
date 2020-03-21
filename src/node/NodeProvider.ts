d2dc54bdb1358fd124666d3d48592afc849a77bb
Alberto Piu <a@lbr.to>
2020-03-21 14:17:11 +0100

    fix: lint

--- src/node/NodeProvider.ts
+++ src/node/NodeProvider.ts
@@ -16,13 +16,13 @@ export class NodeProvider implements INodeProvider {
     this.pathValidator.validateDirectory(location);
 
     // todo here it would be a good place to call plugins for params
-    return new Post(location, {}, nodeFinder);
+    return new Post(location, [], nodeFinder);
   }
 
   provideAttachment(location: IPath, nodeFinder: INodeFinder): IAttachment {
     this.pathValidator.validateDirectory(location);
 
     // todo here it would be a good place to call plugins for params
-    return new Attachment(location, {}, nodeFinder);
+    return new Attachment(location, [], nodeFinder);
   }
 }
