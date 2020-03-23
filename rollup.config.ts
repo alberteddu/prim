15267956ec03bc8c24372a0f8aca091a418b18df
Alberto Piu <a@lbr.to>
2020-03-23 12:42:09 +0100

    fix: add cjs build

--- rollup.config.ts
+++ rollup.config.ts
@@ -9,7 +9,10 @@ const pkg = require('./package.json');
 
 export default {
   input: `src/index.ts`,
-  output: [{ file: pkg.module, format: 'es', sourcemap: true }],
+  output: [
+    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
+  ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: ['path', 'fs'],
   watch: {
